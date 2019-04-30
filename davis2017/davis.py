Fixed void mask computation

davis2017/davis.py
@@ -9,6 +9,7 @@ class DAVIS(object):
     SUBSET_OPTIONS = ['train', 'val', 'test-dev', 'test-challenge']
     TASKS = ['semi-supervised', 'unsupervised']
     DATASET_WEB = 'https://davischallenge.org/davis2017/code.html'
+    VOID_LABEL = 255
 
     def __init__(self, root, task='unsupervised', subset='val', sequences='all', resolution='480p', codalab=False):
         """
@@ -73,8 +74,6 @@ class DAVIS(object):
         obj_id = []
         for i, obj in enumerate(self.sequences[sequence][obj_type]):
             all_objs[i, ...] = np.array(Image.open(obj))
-            if obj_type == 'masks':
-                all_objs[i, all_objs[i, ...] == 255] = 0
             obj_id.append(''.join(obj.split('/')[-1].split('.')[:-1]))
         return all_objs, obj_id
 
@@ -83,12 +82,20 @@ class DAVIS(object):
 
     def get_all_masks(self, sequence, separate_objects_masks=False):
         masks, masks_id = self._get_all_elements(sequence, 'masks')
+        masks_void = np.zeros_like(masks)
+
+        # Separate void and object masks
+        for i in range(masks.shape[0]):
+            masks_void[i, ...] = masks[i, ...] == 255
+            masks[i, masks[i, ...] == 255] = 0
+
         if separate_objects_masks:
             num_objects = int(np.max(masks[0, ...]))
             tmp = np.ones((num_objects, *masks.shape))
             tmp = tmp * np.arange(1, num_objects + 1)[:, None, None, None]
             masks = (tmp == masks[None, ...])
-        return masks > 0, masks_id
+            masks = masks > 0
+        return masks, masks_void, masks_id
 
     def get_sequences(self):
         for seq in self.sequences:
@@ -103,11 +110,11 @@ if __name__ == '__main__':
 
     for s in subsets:
         dataset = DAVIS(root='/home/csergi/scratch2/Databases/DAVIS2017_private', subset=s)
-        for sequence in dataset.get_sequences():
-            g = dataset.get_frames(sequence)
+        for seq in dataset.get_sequences():
+            g = dataset.get_frames(seq)
             img, mask = next(g)
             plt.subplot(2, 1, 1)
-            plt.title(sequence)
+            plt.title(seq)
             plt.imshow(img)
             plt.subplot(2, 1, 2)
             plt.imshow(mask)

davis2017/evaluation.py
@@ -10,8 +10,6 @@ from davis2017 import utils
 from davis2017.results import Results
 from scipy.optimize import linear_sum_assignment
 
-_VOID_LABEL = 255
-
 
 class DAVISEvaluation(object):
     def __init__(self, davis_root, task, gt_set, sequences='all', codalab=False):
@@ -27,21 +25,20 @@ class DAVISEvaluation(object):
         self.dataset = DAVIS(root=davis_root, task=task, subset=gt_set, sequences=sequences, codalab=codalab)
 
     @staticmethod
-    def _evaluate_semisupervised(all_gt_masks, all_res_masks, metric):
+    def _evaluate_semisupervised(all_gt_masks, all_res_masks, all_void_masks, metric):
         if all_res_masks.shape[0] != all_gt_masks.shape[0]:
             sys.stdout.write("\nIn your PNG files there is an index higher than the number of objects in the sequence!")
             sys.exit()
         j_metrics_res, f_metrics_res = np.zeros(all_gt_masks.shape[:2]), np.zeros(all_gt_masks.shape[:2])
         for ii in range(all_gt_masks.shape[0]):
-            void_masks = all_gt_masks[ii, ...] == _VOID_LABEL
             if 'J' in metric:
-                j_metrics_res[ii, :] = db_eval_iou(all_gt_masks[ii, ...], all_res_masks[ii, ...], void_masks)
+                j_metrics_res[ii, :] = db_eval_iou(all_gt_masks[ii, ...], all_res_masks[ii, ...], all_void_masks)
             if 'F' in metric:
-                f_metrics_res[ii, :] = db_eval_boundary(all_gt_masks[ii, ...], all_res_masks[ii, ...], void_masks)
+                f_metrics_res[ii, :] = db_eval_boundary(all_gt_masks[ii, ...], all_res_masks[ii, ...], all_void_masks)
         return j_metrics_res, f_metrics_res
 
     @staticmethod
-    def _evaluate_unsupervised(all_gt_masks, all_res_masks, metric, max_n_proposals=20):
+    def _evaluate_unsupervised(all_gt_masks, all_res_masks, all_void_masks, metric, max_n_proposals=20):
         if all_res_masks.shape[0] > max_n_proposals:
             sys.stdout.write(f"\nIn your PNG files there is an index higher than the maximum number ({max_n_proposals}) of proposals allowed!")
             sys.exit()
@@ -52,11 +49,10 @@ class DAVISEvaluation(object):
         f_metrics_res = np.zeros((all_res_masks.shape[0], all_gt_masks.shape[0], all_gt_masks.shape[1]))
         for ii in range(all_gt_masks.shape[0]):
             for jj in range(all_res_masks.shape[0]):
-                void_masks = all_gt_masks[ii, ...] == _VOID_LABEL
                 if 'J' in metric:
-                    j_metrics_res[jj, ii, :] = db_eval_iou(all_gt_masks[ii, ...], all_res_masks[jj, ...], void_masks)
+                    j_metrics_res[jj, ii, :] = db_eval_iou(all_gt_masks[ii, ...], all_res_masks[jj, ...], all_void_masks)
                 if 'F' in metric:
-                    f_metrics_res[jj, ii, :] = db_eval_boundary(all_gt_masks[ii, ...], all_res_masks[jj, ...], void_masks)
+                    f_metrics_res[jj, ii, :] = db_eval_boundary(all_gt_masks[ii, ...], all_res_masks[jj, ...], all_void_masks)
         if 'J' in metric and 'F' in metric:
             all_metrics = (np.mean(j_metrics_res, axis=2) + np.mean(f_metrics_res, axis=2)) / 2
         else:
@@ -81,14 +77,14 @@ class DAVISEvaluation(object):
         # Sweep all sequences
         results = Results(root_dir=res_path)
         for seq in tqdm(list(self.dataset.get_sequences())):
-            all_gt_masks, all_masks_id = self.dataset.get_all_masks(seq, True)
+            all_gt_masks, all_void_masks, all_masks_id = self.dataset.get_all_masks(seq, True)
             if self.task == 'semi-supervised':
                 all_gt_masks, all_masks_id = all_gt_masks[:, 1:-1, :, :], all_masks_id[1:-1]
             all_res_masks = results.read_masks(seq, all_masks_id)
             if self.task == 'unsupervised':
-                j_metrics_res, f_metrics_res = self._evaluate_unsupervised(all_gt_masks, all_res_masks, metric)
+                j_metrics_res, f_metrics_res = self._evaluate_unsupervised(all_gt_masks, all_res_masks, all_void_masks, metric)
             elif self.task == 'semi-supervised':
-                j_metrics_res, f_metrics_res = self._evaluate_semisupervised(all_gt_masks, all_res_masks, metric)
+                j_metrics_res, f_metrics_res = self._evaluate_semisupervised(all_gt_masks, all_res_masks, None, metric)
             for ii in range(all_gt_masks.shape[0]):
                 seq_name = f'{seq}_{ii+1}'
                 if 'J' in metric:

davis2017/metrics.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
     results = Results(root_dir='examples/osvos')
     # Test timing F measure
     for seq in dataset.get_sequences():
-        all_gt_masks, all_masks_id = dataset.get_all_masks(seq, True)
+        all_gt_masks, _, all_masks_id = dataset.get_all_masks(seq, True)
         all_gt_masks, all_masks_id = all_gt_masks[:, 1:-1, :, :], all_masks_id[1:-1]
         all_res_masks = results.read_masks(seq, all_masks_id)
         f_metrics_res = np.zeros(all_gt_masks.shape[:2])